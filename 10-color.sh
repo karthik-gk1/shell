b2c2c904a01a8f9ac6b618b2ee330e2b9e220d62
Ganesh <sriganesh.gk1@gmail.com>
2025-03-15 09:15:59 +0530

    script updated

---
+++ 10-color.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+VALIDATE() {
+    if  [ $1 -ne 0 ]
+    then 
+        echo "Installing $2... $R Falied $W"
+        exit 1
+    else 
+        echo "Installing $2... $G Success $W"
+    fi
+}
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you are not root user"
+    exit 1 # other than zero manual exit status
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? Mysql
+else 
+    echo "Mysql installed... $Y already $W"
+fi 
