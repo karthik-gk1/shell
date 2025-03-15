6ca938f3776ede06ade8949f933f6a73c159f698
Ganesh <sriganesh.gk1@gmail.com>
2025-03-15 08:44:25 +0530

    script updated

---
+++ 08-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then 
+    echo "you are not root user"
+    exit 1 # other than zero manual exit status
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0]
+then 
+    echo "Installing Mysql... Failure"
+    exit 1
+else 
+    echo "Installing Mysql... Success"
+fi
