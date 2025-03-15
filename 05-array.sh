454d9ed8e80125840b9b715e1f97ade7975734fd
Ganesh <sriganesh.gk1@gmail.com>
2025-03-15 08:31:50 +0530

    script updated

--- 05-array.sh
+++ 05-array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-MOVIES=("pushpa" "rrr" "devara")
+MOVIES=("Pushpa" "RRR" "Devara")
 
 
 echo "First movie: ${MOVIES[0]}"
 echo "Second movie: ${MOVIES[1]}"
 echo "Third movie: ${MOVIES[2]}"
 
-echo "All movies are: ${MOVIES[@]}
+echo "All movies are: ${MOVIES[$@]}
