17e1ba16ac37900c15939caa8da3f4fac2797dd5
Ganesh <sriganesh.gk1@gmail.com>
2025-03-15 10:05:37 +0530

    multiple package install script updated

--- 12-loop.sh
+++ 12-loop.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-logs"
+LOGS_FOLDER="/var/log/shell-log"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -26,7 +26,7 @@ CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
         echo "ERROR:: You must have sudo access to execute this script"
-        exit 1 #other than 0
+        exit 1
     fi
 }
 
@@ -45,3 +45,5 @@ do
         echo -e "$package is already $Y ... INSTALLED $N"
     fi
 done
+
+#sudo sh 12-loop.sh myqsl gcc nginx
