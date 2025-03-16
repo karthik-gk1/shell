2ff8a3e9bae5070b97f0f209dbf3d84555a43c4e
Ganesh <sriganesh.gk1@gmail.com>
2025-03-16 18:34:53 +0530

    install dnf package shell script

---
+++ 08-install-package.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you are not root user"
+    exit 1 # other than zero manual exit status
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if  [ $? -ne 0 ]
+    then 
+        echo "Installing Mysql... Falied"
+        exit 1
+    else 
+        echo "Installing Mysql... Success"
+    fi
+else 
+    echo "Mysql already... installed"
+fi 
+
+# dnf install mysql -y
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "Installing Mysql... Failure"
+#     exit 1
+# else 
+#     echo "Installing Mysql... Success"
+# fi
