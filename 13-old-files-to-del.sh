874d1f74a8e0ee32d679eb95d8a7dd6db945a29c
Ganesh <sriganesh.gk1@gmail.com>
2025-03-16 19:24:55 +0530

    Delete 14days old files shellscript as crontab job

--- 13-old-files-to-del.sh
+++ 13-old-files-to-del.sh
@@ -6,23 +6,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR="/home/ec2-user/app-logs"
-
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-
+SOURCE_DIR="/home/ec2-user/app-logs"
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
@@ -34,4 +23,8 @@ do
     echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
     rm -rf $filepath
     echo "Deleted file: $filepath"
-done <<< $FILES_TO_DELETE
+done <<< $FILES_TO_DELETE  
+
+
+# crontab -e --> schedule the scripts as per your timeline
+# *	 *	*	*	*
